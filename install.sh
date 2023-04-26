54f28e6a3538e81aa2d46d3abeaf4140d05b057e
zyangbi <62045232+zyangbi@users.noreply.github.com>
2023-04-26 20:34:24 +0800

    install.sh for nvim

--- install.sh
+++ install.sh
@@ -1,19 +1,30 @@
 DOTFILES=$HOME/code/dotfiles
 
-# Install Antigen
-if [ ! -d $HOME/.antigen ] ; then
-    mkdir $HOME/.antigen
+# antigen
+if [ ! -d $HOME/.antigen ]; then
+	mkdir $HOME/.antigen
 fi
-if [ ! -f $HOME/.antigen/antigen.zsh ] ; then
-    curl -L git.io/antigen > $HOME/.antigen/antigen.zsh
+if [ ! -f $HOME/.antigen/antigen.zsh ]; then
+	echo "Install Antigen"
+	curl -L git.io/antigen >$HOME/.antigen/antigen.zsh
 fi
 
+# zsh
 ln -sf "$DOTFILES/zsh/.zshrc" "$HOME"
 ln -sf "$DOTFILES/zsh/.p10k.zsh" "$HOME"
 
+# tmux
 ln -sf "$DOTFILES/tmux/tmux.conf" "$HOME/.tmux.conf"
 #git clone https://github.com/tmux-plugins/tpm "$DOTFILES/vendor/tpm"
 
+# vim
 ln -sf "$DOTFILES/vim/.vimrc" "$HOME"
 
-ln -sf "$DOTFILES/nvim/lua" "$HOME/.config/nvim/lua"
+# nvim (lazyvim)
+if [ ! -f "$HOME/.config/nvim/lua/config/lazy.lua" ]; then
+  echo "Install LazyVim"
+	git clone https://github.com/LazyVim/starter $HOME/.config/nvim
+	rm -rf $HOME/.config/nvim/.git
+fi
+rm -rf $HOME/.config/nvim/lua
+ln -sfT "$DOTFILES/nvim/lua" "$HOME/.config/nvim/lua"
